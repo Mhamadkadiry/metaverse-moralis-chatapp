1e1428f267f3aae00c847be3f8d17e6e120dbde9
mhamadkadiry <mhamad.kadiry1@gmail.com>
2022-03-26 12:58:59 +0200

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Login from '../components/Login'
+import { useMoralis } from 'react-moralis'
+const Home = () => {
+  const { isAuthenticated, logout } = useMoralis()
+
+  if (!isAuthenticated) return <Login />
+  return (
+    <div className="h-screen">
+      <Head>
+        <title>Metaverse</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Welcome to the app</h1>
+      <button onClick={logout}>Logout</button>
+    </div>
+  )
+}
+
+export default Home
